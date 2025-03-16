39bd253cca36462c7d0a99c077974dc23dcd4bf5
bjleatherman <56315723+bjleatherman@users.noreply.github.com>
2025-03-16 01:10:53 +0000

    refactor: updated main method to reflect other changes

--- BusShuttleProject/BusShuttle/Program.cs
+++ BusShuttleProject/BusShuttle/Program.cs
@@ -1,33 +1,10 @@
 ﻿namespace BusShuttle;
 
-using System.IO;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Please select mode (driver or manager)");
-        string mode = Console.ReadLine();
-
-        if (mode == "driver")
-        {
-
-            string command;
-
-            do 
-            {
-                System.Console.WriteLine("Enter stop name");
-                string stopName = Console.ReadLine();
-
-                System.Console.WriteLine("Enter number of passengers boarded");
-                int boarded= int.Parse(Console.ReadLine());
-
-                File.AppendAllText("passenger-data.txt", stopName+":"+boarded+Environment.NewLine);
-
-                System.Console.WriteLine("Enter command: (end or continue)");
-                command = Console.ReadLine();
-
-            } while(command != "end");
-        }
+        ConsoleUI consoleUI = new ConsoleUI();
+        consoleUI.Show();
     }
 }
